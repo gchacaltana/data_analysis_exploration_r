d16545c51b3eb97761adae7641983b2899dfef0f
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-07-12 23:56:23 -0500

    add install packages and load dataset

--- data_exploration_titanic.R
+++ data_exploration_titanic.R
@@ -6,3 +6,31 @@ if (packageVersion("devtools") < 1.6) {
   install.packages("devtools")
 }
 devtools::install_github("paulhendricks/titanic")
+
+# Instalando paquetes
+install.packages("rlang")
+install.packages("stringi") 
+install.packages("curl")
+install.packages("tidyverse")
+install.packages("ggplot2")
+devtools::install_github("hrbrmstr/waffle")
+
+# Cargando librerias.
+library(ggplot2)
+library(waffle)
+library(tidyverse)
+library(titanic)
+
+# Cargamos el data frame: Titanic
+data <-titanic_train
+
+# Mostramos primeros 6 registros del data frame
+head(data)
+
+knitr::kable(head(titanic_train))
+
+# Mostramos estructura de la base de datos: data frame
+str(Titanic)
+
+# Resumen de las variables del data frame
+summary(Titanic)

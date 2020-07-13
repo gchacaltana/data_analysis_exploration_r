e706d865f84708da7411681a53c345a98b6fa4ea
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-07-12 23:24:12 -0500

    add data_exploration_titanic_dataset

---
+++ data_exploration_titanic.R
@@ -0,0 +1,8 @@
+# Análisis Exploratorio Titanic
+# Gonzalo Chacaltana
+
+# Instalando paquetes para dataset
+if (packageVersion("devtools") < 1.6) {
+  install.packages("devtools")
+}
+devtools::install_github("paulhendricks/titanic")
